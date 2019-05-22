8bba327172223d8c6be15abacd7f1a487674c77f
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2019-05-22 17:11:01 -0400

    W1D2 end of day commit

--- app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
+++ app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
@@ -14,6 +14,7 @@ import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
