9a5db63137148aca19e09d96bcf395bacbea7763
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2019-05-23 12:30:28 -0400

    minor camera behaviour, midday commit

--- app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
+++ app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
@@ -1,5 +1,6 @@
 package griffmedia.ghazal.liveprint;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -54,4 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void openCamera(View v) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
+
 }
