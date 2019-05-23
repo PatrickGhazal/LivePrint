c546c247af656adf6fc1cad04801f806f3c5fe58
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2019-05-23 15:00:13 -0400

    started building first page

--- app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
+++ app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
@@ -15,7 +15,6 @@ import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
