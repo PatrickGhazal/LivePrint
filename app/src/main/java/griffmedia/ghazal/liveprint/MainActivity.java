b90b66b607c99acab9483e8a2b5e3ebbf8665933
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2019-05-27 13:09:18 -0400

    Responsive gap sizes

--- app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
+++ app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
@@ -7,15 +7,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.LinkMovementMethod;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final double gapPerc1 = 2.06;
+    private static final double gapPerc2 = 3.62;
+    private static final double gapPerc34 = 4.18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         TextView getBookTV = (TextView) findViewById(R.id.get_book_text);
         getBookTV.setMovementMethod(LinkMovementMethod.getInstance());
+
+        setGapSizes();
     }
 
     @Override
@@ -55,6 +63,34 @@ public class MainActivity extends AppCompatActivity {
     public void openCamera(View v) {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(intent);
+    }
+
+    private void setGapSizes() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        double fullWindowHeight = displayMetrics.heightPixels + 0.0;
+        double density = displayMetrics.density;
+
+        View v1 = findViewById(R.id.empty_block_1);
+        View v2 = findViewById(R.id.empty_block_2);
+        View v3 = findViewById(R.id.empty_block_3);
+        View v4 = findViewById(R.id.empty_block_4);
+
+        setGapSize(v1, resizeHeight(gapPerc1, fullWindowHeight), density);
+        setGapSize(v2, resizeHeight(gapPerc2, fullWindowHeight), density);
+        setGapSize(v3, resizeHeight(gapPerc34, fullWindowHeight), density);
+        setGapSize(v4, resizeHeight(gapPerc34, fullWindowHeight), density);
+
+    }
+
+    private int resizeHeight(double gapPerc, double windowHeight) {
+        return (int) Math.round(gapPerc * windowHeight / 100.0);
+    }
+
+    private void setGapSize(View view, int heightVal, double density) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) Math.round(heightVal * density);
+        view.setLayoutParams(layoutParams);
     }
 
 }
