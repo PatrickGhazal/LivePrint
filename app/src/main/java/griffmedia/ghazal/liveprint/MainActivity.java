10bd79223a2424256f4b5ad1facac1a9e9cbee19
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2019-05-27 16:47:00 -0400

    clarified gap size computation; fixed wrong background color in API 22

--- app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
+++ app/src/main/java/griffmedia/ghazal/liveprint/MainActivity.java
@@ -1,9 +1,9 @@
 package griffmedia.ghazal.liveprint;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.LinkMovementMethod;
@@ -19,9 +19,10 @@ import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
+    // percentage of screen height that the gap should be
     private static final double gapPerc1 = 2.06;
     private static final double gapPerc2 = 3.62;
-    private static final double gapPerc34 = 4.18;
+    private static final double gapPerc34 = 4.1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
         TextView getBookTV = (TextView) findViewById(R.id.get_book_text);
         getBookTV.setMovementMethod(LinkMovementMethod.getInstance());
 
-        setGapSizes();
+        setFormatting();
+
     }
 
     @Override
@@ -65,29 +67,52 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void setGapSizes() {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        double fullWindowHeight = displayMetrics.heightPixels + 0.0;
-        double density = displayMetrics.density;
-
+    private void setFormatting() {
+        // the four gaps
         View v1 = findViewById(R.id.empty_block_1);
         View v2 = findViewById(R.id.empty_block_2);
         View v3 = findViewById(R.id.empty_block_3);
         View v4 = findViewById(R.id.empty_block_4);
 
-        setGapSize(v1, resizeHeight(gapPerc1, fullWindowHeight), density);
-        setGapSize(v2, resizeHeight(gapPerc2, fullWindowHeight), density);
-        setGapSize(v3, resizeHeight(gapPerc34, fullWindowHeight), density);
-        setGapSize(v4, resizeHeight(gapPerc34, fullWindowHeight), density);
+        setGapSizes(v1, v2, v3, v4);
 
+        // versions before M have a different default background colour
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            v1.setBackgroundColor(Color.parseColor("#EEEEEE"));
+            v2.setBackgroundColor(Color.parseColor("#EEEEEE"));
+            v3.setBackgroundColor(Color.parseColor("#EEEEEE"));
+            v4.setBackgroundColor(Color.parseColor("#EEEEEE"));
+        } else {
+            v1.setBackgroundColor(Color.parseColor("#FAFAFA"));
+            v2.setBackgroundColor(Color.parseColor("#FAFAFA"));
+            v3.setBackgroundColor(Color.parseColor("#FAFAFA"));
+            v4.setBackgroundColor(Color.parseColor("#FAFAFA"));
+        }
     }
 
+    private void setGapSizes(View v1, View v2, View v3, View v4) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        double fullWindowHeight = displayMetrics.heightPixels + 0.0;
+        double density = displayMetrics.density;
+
+        // add a dividing factor for screens with height < 1000 px
+        if (fullWindowHeight < 1000)
+            density /= 1.5;
+
+        setGapHeight(v1, resizeHeight(gapPerc1, fullWindowHeight), density);
+        setGapHeight(v2, resizeHeight(gapPerc2, fullWindowHeight), density);
+        setGapHeight(v3, resizeHeight(gapPerc34, fullWindowHeight), density);
+        setGapHeight(v4, resizeHeight(gapPerc34, fullWindowHeight), density);
+    }
+
+    // compute height from percentage of total height
     private int resizeHeight(double gapPerc, double windowHeight) {
         return (int) Math.round(gapPerc * windowHeight / 100.0);
     }
 
-    private void setGapSize(View view, int heightVal, double density) {
+    // set the newly computed height
+    private void setGapHeight(View view, int heightVal, double density) {
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         layoutParams.height = (int) Math.round(heightVal * density);
         view.setLayoutParams(layoutParams);
